c4135dc44f9576829c14e4581d9f63cd4a635505
Ashish Sharma <ashuysharma@gmail.com>
2022-06-24 14:31:19 +0530

    changed export

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-'use strict'
+"use strict";
 
-import * as AesExports from './Aes'
-import * as NaClExports from './NaCl'
-import * as PgpExports from './Pgp'
-import ResponseBody from './ResponseBody'
-import cryptoHelper from './cryptoHelper'
+import * as AesExports from "./Aes";
+import * as NaClExports from "./NaCl";
+import * as PgpExports from "./Pgp";
+import ResponseBody from "./ResponseBody";
+import cryptoHelper from "./cryptoHelper";
 
-const exportProps = {}
-Object.assign(exportProps, AesExports, NaClExports, PgpExports, { ResponseBody, cryptoHelper, default: undefined })
+const exportProps = {};
+Object.assign(exportProps, AesExports, NaClExports, PgpExports, {
+  ResponseBody,
+  cryptoHelper,
+  default: undefined,
+});
 
-module.exports = exportProps
+export default AesExports;
+module.exports = AesExports;
